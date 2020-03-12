c02691f8455695a9352d168327b5d781fc054f28
Ryan Peek <rapeek@ucdavis.edu>
2020-03-12 09:45:32 -0700

    update and add first tidy tuesday

--- code/2020-03-10-tuition.R
+++ code/2020-03-10-tuition.R
@@ -65,6 +65,8 @@ university_df %>%
 
 # Set Up Model ------------------------------------------------------------
 
+#devtools::install_github("tidymodels/tune")
+library(tune)
 library(tidymodels)
 
 set.seed(1234)
@@ -90,7 +92,6 @@ uni_rec <- recipe(diversity ~ ., data = uni_train) %>%
 
 uni_rec
 
-
 # Run Logistic Regression -------------------------------------------------
 
 # squeeze the recipe with training data so it's transformed
@@ -108,7 +109,6 @@ glm_fit <- glm_spec %>%
 # view
 glm_fit
 
-
 # Run K-Nearest Neighbor --------------------------------------------------
 
 knn_spec <- nearest_neighbor() %>%
@@ -119,7 +119,6 @@ knn_fit <- knn_spec %>%
   fit(diversity ~ ., data = uni_juiced)
 
 knn_fit
-
 
 # Run RPart Decision Tree ------------------------------------------------
 
@@ -138,12 +137,17 @@ set.seed(123)
 folds <- vfold_cv(uni_juiced, strata = diversity)
 
 set.seed(234)
-glm_rs <- glm_spec %>%
+
+glm_rs <- glm_spec %>% 
   fit_resamples(diversity ~ .,
                 folds,
                 metrics = metric_set(roc_auc, sens, spec),
                 control = control_resamples(save_pred = TRUE)
   )
+
+# collect the results
+glm_rs %>%
+  collect_metrics()
 
 set.seed(234)
 knn_rs <- knn_spec %>%
@@ -153,6 +157,9 @@ knn_rs <- knn_spec %>%
                 control = control_resamples(save_pred = TRUE)
   )
 
+knn_rs %>%
+  collect_metrics()
+
 set.seed(234)
 tree_rs <- tree_spec %>%
   fit_resamples(diversity ~ .,
@@ -160,3 +167,60 @@ tree_rs <- tree_spec %>%
                 metrics = metric_set(roc_auc, sens, spec),
                 control = control_resamples(save_pred = TRUE)
   )
+
+tree_rs %>%
+  collect_metrics()
+
+# check it out!
+tree_rs
+
+
+# Visualize ---------------------------------------------------------------
+
+glm_rs %>%
+  unnest(.predictions) %>%
+  mutate(model = "glm") %>%
+  bind_rows(knn_rs %>%
+              unnest(.predictions) %>%
+              mutate(model = "knn")) %>%
+  bind_rows(tree_rs %>%
+              unnest(.predictions) %>%
+              mutate(model = "rpart")) %>%
+  group_by(model) %>%
+  roc_curve(diversity, .pred_high) %>%
+  ggplot(aes(x = 1 - specificity, y = sensitivity, color = model)) +
+  geom_line(size = 1.5) +
+  geom_abline(
+    lty = 2, alpha = 0.5,
+    color = "gray50",
+    size = 1.2
+  )
+
+
+# arrange by estimate
+glm_fit %>%
+  tidy() %>%
+  arrange(-estimate)
+
+
+# Predict -----------------------------------------------------------------
+
+# Use test data to do unbiased check on how we can expect this model to perform on new data.
+# bake() our recipe using the testing set to apply the same preprocessing steps that we used on the training data
+
+glm_fit %>%
+  predict(
+    new_data = bake(uni_rec, uni_test),
+    type = "prob"
+  ) %>%
+  mutate(truth = uni_test$diversity) %>%
+  roc_auc(truth, .pred_high)  
+
+# check specificity
+glm_fit %>%
+  predict(
+    new_data = bake(uni_rec, new_data = uni_test),
+    type = "class"
+  ) %>%
+  mutate(truth = uni_test$diversity) %>%
+  spec(truth, .pred_class)
